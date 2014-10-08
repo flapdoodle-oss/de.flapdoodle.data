1b85d035e3a3e4c57af0ffcaa4c0886f55cf57fd
Jan Bernitt <jan.bernitt@gmx.de>
2014-10-08 15:47:55 +0200

    more tests...

--- src/test/java/de/flapdoodle/data/TestDataset.java
+++ src/test/java/de/flapdoodle/data/TestDataset.java
@@ -1,12 +1,14 @@
 package de.flapdoodle.data;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
 public class TestDataset {
 
 	static final Property<String> FOO = Property.named("foo", String.class);
+	static final Property<Integer> X = Property.named("x", Integer.class);
 	
 	@Test
 	public void thatPutAndGetBasicallyWorks() {
@@ -14,5 +16,9 @@ public class TestDataset {
 		ds = ds.put(FOO, "bar");
 		
 		assertEquals("bar", ds.get(FOO));
+		
+		Dataset ds2 = ds.put(X, 42);
+		assertEquals(42, ds2.get(X).intValue());
+		assertNull(ds.get(X));
 	}
 }
